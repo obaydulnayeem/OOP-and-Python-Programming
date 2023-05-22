b501e6ea70ea01c08db9614a9261d7483966cc1f
Nayeem <nayeem.cse6.bu@gmail.com>
2023-05-22 12:22:22 +0600

    added

---
+++ intro_to_python.py
@@ -0,0 +1,81 @@
+# BASIC INFO===================================
+"""
+    Uses of python:
+        - Data science
+        - AI
+        - Machine learning
+        - web crawlling, web development
+
+    Others Info:
+        - Indentation is so important in python
+"""
+
+
+# PRINT==========================
+#print('kopa python kopa')
+#print(4534)
+
+
+# VARIABLE=======================
+age = 45
+a = 3.3
+interest_rate = 2.3
+name = "Nayeem"
+district = 'Barishal'
+is_single = True
+is_sleeping = False
+#print(age + a)
+
+
+# TYPE===================================================
+# print(type(age))
+# print(type(district))
+# print(type(is_single))
+
+
+# COMMENT====================================================
+"""
+Ctrl + / --> single line
+Alt + Shift + A --> multi line / doc string
+"""
+
+# STRING========================================================================================
+# print('Kodom Ali' + ' ' + 'Kacha Badam')
+text = f"Kodom Ali {age} living in {district}. Interesting with rate {interest_rate}%"  # fstring
+# print(text)
+
+
+# INPUT===================================================================
+# print('Now I need money')
+# input()
+# input('Give me some money: ')
+# money = input('Give me some money: ')
+# print("Here is your money:", money)
+
+# first_money = input('Kodom Ali, Dosto kichu tk dey: ')
+# second_money = input('Peyara Begum, Dosto kichu tk dey: ')
+# print(type(first_money))
+# by default the input from user will be string type
+# first_money_int = int(first_money)
+#print(type(first_money_int))
+# second_money_int = int(second_money)
+# print("Money I got from kodom", first_money, 'and from peyara', second_money)
+# total = first_money_int + second_money_int
+# print('Total Money I got: ', total)
+
+
+# OPERATORS=====================================================================
+# +, -, *, /, %, //, **
+num1 = 15
+num2 = 6
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(10/3)
+print(10%3) # reminder / vag sesh
+print(10//3) # purno vag fol
+print(5**2) ## power
+# COMPARISON:
+# >, <, >=, <=, ==, !==
+# not available: ++ --
