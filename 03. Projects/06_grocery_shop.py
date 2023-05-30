e1944a01124db21a4e3f64df450090f300817bac
Obaydul Hasan Nayeem <nayeem.cse6.bu@gmail.com>
2023-05-30 11:58:38 +0600

    added

---
+++ 03. Projects/06_grocery_shop.py
@@ -0,0 +1,37 @@
+class Product:
+    def __init__(self, product_name, product_id, price, expired_date) -> None:
+        self.product_name = product_name
+        self.product_id = product_id
+        self.price = price
+        self.expire_date = expired_date
+
+    def __repr__(self):
+        return f'Product Name: {self.product_name}, Id: {self.product_id}, Price: {self.price}, Expired Date: {self.expire_date}'
+    
+class Shop: 
+    def __init__(self, name, location, propiter, trade_license) -> None:
+        self.name = name
+        self.location = location
+        self.propiter = propiter
+        self.trade_license = trade_license
+
+        self.products = []
+
+    def add_product(self, product_name, product_id, price, expired_date): # method
+        product = Product(product_name, product_id, price, expired_date)
+        self.products.append(product)
+
+    def __repr__(self):
+        shop_info = f'Shop Name:{self.name}'
+
+        product_info = ""
+        for product in self.products:
+            product_info += repr(product)
+
+        return shop_info + product_info
+
+swapno = Shop('Swapno Supar Shop', 'Barishal', 'Nayeem', '645654')
+swapno.add_product('Rice', '343', '65', '20.02.2024')
+
+# print(swapno.name)
+print(swapno)
